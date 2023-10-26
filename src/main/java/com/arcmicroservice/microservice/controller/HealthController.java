aa9b8fc926435ab845723afe39a50fde80039e17
Igor <nemykin@i-teco.ru>
2023-10-26 17:53:27 +0300

    D1: add health controller with ping

---
+++ src/main/java/com/arcmicroservice/microservice/controller/HealthController.java
@@ -0,0 +1,21 @@
+package com.arcmicroservice.microservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Asus 25.10.2023
+ */
+@RestController
+@Slf4j
+public class HealthController {
+
+    @RequestMapping(value = "/health", method = RequestMethod.GET)
+    public ResponseEntity ping() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
